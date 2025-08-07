8dcc04e1dea5317b492b12ce28d0f3594136a766
Zhang2134 <3193603481@qq.com>
2025-08-07 15:25:05 +0800

    Update test.sh

--- PyTorch/contrib/Classification/transformerxl/run_scripts/test.sh
+++ PyTorch/contrib/Classification/transformerxl/run_scripts/test.sh
@@ -3,18 +3,21 @@ script_path=$(dirname $(readlink -f "$0"))
 echo "当前脚本路径: $script_path"
 
 #安装依赖
+cd .. 
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install numpy==1.24.3
+pip install huggingface_hub
+pip install parameterized
+cd $script_path
 git clone https://gitee.com/xiwei777/tcap_dllogger.git
 cd tcap_dllogger
 python setup.py install
 cd ..
-cd .. 
-pip install -r requirements.txt
-cd run_scripts
 
 
 #执行训练
 
-python run_transformerxl.py --nproc-per-node 1 2>&1 | tee sdaa.log
+python run_transformerxl.py 2>&1 | tee sdaa.log
 
 
 # 生成loss对比图
